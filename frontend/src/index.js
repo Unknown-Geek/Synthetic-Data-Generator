9f90f55d77bac1bbc9139dd1f556271fc109bc9b
Shravan Nander Pandala <97742905+Unknown-Geek@users.noreply.github.com>
2025-01-08 01:01:16 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './output.css';
 import UploadForm from './UploadForm';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.render(
   <div className="dark">
     <UploadForm />
+    <Analytics />
   </div>,
   document.getElementById('root')
 );
